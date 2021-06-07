3508e94ed1248d6544e6fe7c15b954dcacf8329e
Bojan <bl19@tu-clausthal.de>
2021-06-07 20:25:01 +0200

    fix scripts

--- scripts.js
+++ scripts.js
@@ -1,92 +1,91 @@
 
 // Accordion menu script
-<script>
-	var acc = document.getElementsByClassName("accordion");
-	var i;
 
-	for (i = 0; i < acc.length; i++) {
-	acc[i].addEventListener("click", function() {
-		this.classList.toggle("active");
-		var panel = this.nextElementSibling;
-		if (panel.style.maxHeight) {
-		panel.style.maxHeight = null;
-		} else {
-		panel.style.maxHeight = panel.scrollHeight + "px";
-		} 
-	});
-	}
-</script>
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+acc[i].addEventListener("click", function() {
+	this.classList.toggle("active");
+	var panel = this.nextElementSibling;
+	if (panel.style.maxHeight) {
+	panel.style.maxHeight = null;
+	} else {
+	panel.style.maxHeight = panel.scrollHeight + "px";
+	} 
+});
+}
+
 
 // Comment box script
-<script 
-	src="https://my.remarkbox.com/static/js/iframe-resizer/iframeResizer.min.js">
-</script>
-<script>
-	var rb_owner_key = "1b94e84a-8979-11eb-9664-040140774501";
-	var thread_uri = window.location.href;
-	var thread_title = window.document.title;
-	var thread_fragment = window.location.hash;
 
-	// rb owner was here.
-	var rb_src = "https://my.remarkbox.com/embed" + 
-		"?rb_owner_key=" + rb_owner_key +
-		"&thread_title=" + encodeURI(thread_title) +
-		"&thread_uri=" + encodeURIComponent(thread_uri) + 
-		thread_fragment;
+var rb_owner_key = "1b94e84a-8979-11eb-9664-040140774501";
+var thread_uri = window.location.href;
+var thread_title = window.document.title;
+var thread_fragment = window.location.hash;
 
-	function create_remarkbox_iframe() {
-		var ifrm = document.createElement("iframe");
-		ifrm.setAttribute("id", "remarkbox-iframe");
-		ifrm.setAttribute("scrolling", "no");
-		ifrm.setAttribute("src", rb_src);
-		ifrm.setAttribute("frameborder", "0");
-		ifrm.setAttribute("tabindex", "0");
-		ifrm.setAttribute("title", "Remarkbox");
-		ifrm.style.width = "75%";
-		document.getElementById("remarkbox-div").appendChild(ifrm);
+// rb owner was here.
+var rb_src = "https://my.remarkbox.com/embed" + 
+	"?rb_owner_key=" + rb_owner_key +
+	"&thread_title=" + encodeURI(thread_title) +
+	"&thread_uri=" + encodeURIComponent(thread_uri) + 
+	thread_fragment;
+
+function create_remarkbox_iframe() {
+	var ifrm = document.createElement("iframe");
+	ifrm.setAttribute("id", "remarkbox-iframe");
+	ifrm.setAttribute("scrolling", "no");
+	ifrm.setAttribute("src", rb_src);
+	ifrm.setAttribute("frameborder", "0");
+	ifrm.setAttribute("tabindex", "0");
+	ifrm.setAttribute("title", "Remarkbox");
+	ifrm.style.width = "75%";
+	document.getElementById("remarkbox-div").appendChild(ifrm);
+}
+create_remarkbox_iframe();
+iFrameResize(
+{
+	checkOrigin: ["https://my.remarkbox.com"],
+	inPageLinks: true,
+	initCallback: function(e) {e.iFrameResizer.moveToAnchor(thread_fragment)}
+},
+document.getElementById("remarkbox-iframe")
+);
+
+
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+
+window.onscroll = function() {
+	scrollFunction()
+	scrollFunction2()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+		document.getElementById("header").style.fontSize = "10px";
+		document.getElementById("nav").style.top = "40px";
+		document.getElementById("article").style.paddingTop = "40px";
+	} else {
+		document.getElementById("header").style.fontSize = "30px";
+		document.getElementById("nav").style.top = "130px";
+		document.getElementById("article").style.paddingTop = "130px";
 	}
-	create_remarkbox_iframe();
-	iFrameResize(
-	{
-		checkOrigin: ["https://my.remarkbox.com"],
-		inPageLinks: true,
-		initCallback: function(e) {e.iFrameResizer.moveToAnchor(thread_fragment)}
-	},
-	document.getElementById("remarkbox-iframe")
-	);
-</script>
+}
 
-<script>
-	// When the user scrolls down 50px from the top of the document, resize the header's font size
-	window.onscroll = function() {
-		scrollFunction()
-		scrollFunction2()
-	};
-	
-	function scrollFunction() {
-		if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-			document.getElementById("header").style.fontSize = "10px";
-			document.getElementById("nav").style.top = "40px";
-			document.getElementById("article").style.paddingTop = "40px";
-		} else {
-			document.getElementById("header").style.fontSize = "30px";
-			document.getElementById("nav").style.top = "130px";
-			document.getElementById("article").style.paddingTop = "130px";
-		}
+function scrollFunction2() {
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+		document.getElementById("textdefault").style.opacity = "0";
+		document.getElementById("textscroll").style.opacity = "1";
+	} else {
+		document.getElementById("textdefault").style.opacity = "1";
+		document.getElementById("textscroll").style.opacity = "0";
 	}
+}
 
-	function scrollFunction2() {
-		if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-			document.getElementById("textdefault").style.opacity = "0";
-			document.getElementById("textscroll").style.opacity = "1";
-		} else {
-			document.getElementById("textdefault").style.opacity = "1";
-			document.getElementById("textscroll").style.opacity = "0";
-		}
-	}
-</script>
 
-/* Back to top button script not needed for now 
+/* 
+//Back to top button script not needed for now 
+
 <script>
 	//Get the button
 	var mybutton = document.getElementById("myBtn");
