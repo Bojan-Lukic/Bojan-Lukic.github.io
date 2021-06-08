c8432175fee54509c837a3cb7cabc097664df8e7
Bojan <bl19@tu-clausthal.de>
2021-06-08 08:39:17 +0200

    split html code into subdirectories

--- scripts.js
+++ scripts.js
@@ -17,6 +17,36 @@ acc[i].addEventListener("click", function() {
 }
 
 
+// When the user scrolls down 50px from the top of the document, resize the header's font size
+
+window.onscroll = function() {
+	scrollFunction()
+	scrollFunction2()
+};
+
+function scrollFunction() {
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+		document.getElementById("header").style.fontSize = "10px";
+		document.getElementById("nav").style.top = "40px";
+		document.getElementById("article").style.paddingTop = "40px";
+	} else {
+		document.getElementById("header").style.fontSize = "30px";
+		document.getElementById("nav").style.top = "130px";
+		document.getElementById("article").style.paddingTop = "130px";
+	}
+}
+
+function scrollFunction2() {
+	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
+		document.getElementById("textdefault").style.opacity = "0";
+		document.getElementById("textscroll").style.opacity = "1";
+	} else {
+		document.getElementById("textdefault").style.opacity = "1";
+		document.getElementById("textscroll").style.opacity = "0";
+	}
+}
+
+
 // Comment box script
 
 var rb_owner_key = "1b94e84a-8979-11eb-9664-040140774501";
@@ -51,36 +81,6 @@ iFrameResize(
 },
 document.getElementById("remarkbox-iframe")
 );
-
-
-// When the user scrolls down 50px from the top of the document, resize the header's font size
-
-window.onscroll = function() {
-	scrollFunction()
-	scrollFunction2()
-};
-
-function scrollFunction() {
-	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-		document.getElementById("header").style.fontSize = "10px";
-		document.getElementById("nav").style.top = "40px";
-		document.getElementById("article").style.paddingTop = "40px";
-	} else {
-		document.getElementById("header").style.fontSize = "30px";
-		document.getElementById("nav").style.top = "130px";
-		document.getElementById("article").style.paddingTop = "130px";
-	}
-}
-
-function scrollFunction2() {
-	if (document.body.scrollTop > 150 || document.documentElement.scrollTop > 150) {
-		document.getElementById("textdefault").style.opacity = "0";
-		document.getElementById("textscroll").style.opacity = "1";
-	} else {
-		document.getElementById("textdefault").style.opacity = "1";
-		document.getElementById("textscroll").style.opacity = "0";
-	}
-}
 
 
 /* 
